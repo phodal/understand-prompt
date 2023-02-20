f7cfd245fd7a012fa3f146c5ff456bc74701ae87
Phodal Huang <h@phodal.com>
2023-02-20 12:06:31 +0800

    docs: add translate for examples

--- codeai.py
+++ codeai.py
@@ -5,15 +5,11 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 # model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-6B-mono")
 
 # 或者用下载到本地的版本
-tokenizer = AutoTokenizer.from_pretrained("./diff-codegen-6b-v2/")
-model = AutoModelForCausalLM.from_pretrained("./diff-codegen-6b-v2/pytorch_model.bin")
+tokenizer = AutoTokenizer.from_pretrained("/Volumes/game/codeai/ai-research/diff-codegen-6b-v2")
+model = AutoModelForCausalLM.from_pretrained("/Volumes/game/codeai/ai-research/diff-codegen-6b-v2")
 
 text = "def hello_world():"
 input_ids = tokenizer(text, return_tensors="pt").input_ids
 
 generated_ids = model.generate(input_ids, max_length=128)
 print(tokenizer.decode(generated_ids[0], skip_special_tokens=True))
-
-
-
-#%%
